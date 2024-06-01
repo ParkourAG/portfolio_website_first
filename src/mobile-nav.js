1360aa489772443eaa38f8496a428c7493ebe086
ParkourAG <guchhaitanubrata@gmail.com>
2024-06-01 23:17:21 +0530

    .

--- src/mobile-nav.js
+++ src/mobile-nav.js
@@ -1,7 +1,27 @@
-const headerBtn= document.querySelector(".header__bars");
-const mobileNav= document.querySelector(".mobile-nav");
+const headerBtn = document.querySelector(".header__bars");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileLinks= document.querySelectorAll(".mobile-nav__link");
 
-headerBtn.addEventListener("click", ()=>{
-    mobileNav.style.display= "flex";
-    
+let isMobileNavOpen = false;
+
+headerBtn.addEventListener("click", () => {
+    isMobileNavOpen = !isMobileNavOpen;
+    if (isMobileNavOpen) {
+        mobileNav.style.display = "flex";
+        document.body.style.overflowX = "";
+        document.body.style.overflow = "hidden";
+    }else{
+        mobileNav.style.display = "none";
+        document.body.style.overflowX = "hidden";
+        document.body.style.overflow = "";
+    }
+})
+
+mobileLinks.forEach((links)=>{
+    links.addEventListener("click", ()=>{
+        isMobileNavOpen= false;
+        mobileNav.style.display = "none";
+        document.body.style.overflowX = "hidden";
+        document.body.style.overflow = "";
+    })
 })
