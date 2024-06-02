d03b9fee645b01909fc96230b81794f29c3e2d2b
ParkourAG <guchhaitanubrata@gmail.com>
2024-06-02 13:17:25 +0530

    .

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 function resizeParticles() {
     var particlesContainer = document.querySelector(".background");
     particlesContainer.style.height = document.body.scrollHeight+"px";
-    // particlesContainer.style.width = document.body.scrollHeight+"px";
 }
 
 resizeParticles();
